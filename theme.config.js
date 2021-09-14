1e5a370f951f1a01fee27747d6b9d694524a5a3c
Abbas Ali <abbasali.mehtar@gmail.com>
2021-09-14 20:38:03 +0530

    bmc

--- theme.config.js
+++ theme.config.js
@@ -27,7 +27,7 @@ export default {
       <meta name="og:image" content="gizmolab-logo.png" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <script async src="https://arc.io/widget.min.js#BdZRG1XA"></script>
-      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="gizmolab" data-description="Support me on Buy me a coffee!" data-message="" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="g1zmo" data-description="Support me on Buy me a coffee!" data-message="" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       <link
         rel="apple-touch-icon"
         sizes="180x180"
